725f531383cabb46c53da1ea6b3521b1a573949c
Santosh Behera01 <Santosh.Behera01@godigit.com>
2023-01-22 22:48:43 +0530

    Local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ls';
+  
+  session : any;
+
+  //for storing data in local storage
+saveData(){
+  let data = { id: 1, name: 'xyz'};
+  localStorage.setItem('session',JSON.stringify(data))
+}
+//for showing data in ui
+loadData(){
+  let data : any = localStorage.getItem('session');
+  this.session = JSON.parse(data);
+  alert(data);
+}
+
+//for remove data from local storage
+remove(){
+  localStorage.removeItem("session");
+}
+
 }
